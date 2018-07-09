7310d2775a2c5e8fd685dab77f8f26ddd33dd618
rex <rex.chiu@outlook.com>
2018-07-09 13:19:50 -0400

    added functionality to save an avatar URL into a .jpg

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,10 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb){
+function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: 'https://api.github.com/repos/' + repoOwner + "/" + repoName + "/contributors",
         headers: {
@@ -11,21 +12,31 @@ function getRepoContributors(repoOwner, repoName, cb){
             'Authorization': secrets.GITHUB_TOKEN
         }
     };
-    
-    request(options, function(err, res, body) {
+
+    request(options, function (err, res, body) {
         var obj = JSON.parse(body);
         cb(err, obj);
     });
 }
 
-function iterateAvatars(err, obj){
-    if (err){
+function iterateAvatars(err, obj) {
+    if (err) {
         throw err;
     }
 
-    for (var index in obj){
+    for (var index in obj) {
         console.log(obj[index].avatar_url);
     }
 }
 
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+        .on('error', function (err) {
+            throw err;
+        })
+        .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("jquery", "jquery", iterateAvatars);
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
