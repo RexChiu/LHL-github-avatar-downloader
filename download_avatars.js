4276035b651bd452479f6d35ae7c1208274df680
rex <rex.chiu@outlook.com>
2018-07-09 12:58:42 -0400

    added url parsing for request url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb){
+    var requestURL = 'https://api.github.com/repos/' + repoOwner + "/" + repoName + "/contributors";
+    console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
