5143dea0c3cdb9a712ca4370d1d18b4a675fb9d9
rex <rex.chiu@outlook.com>
2018-07-09 16:54:22 -0400

    added framework for recommend functionality

---
+++ recommend.js
@@ -0,0 +1,24 @@
+var dotenvResult = require('dotenv').config();
+var dotenv = require('dotenv');
+
+var request = require('request');
+var fs = require('fs');
+var gitHubAPI = require('./githubAPI.js');
+
+console.log('Welcome to the GitHub Recommend Thingy!');
+
+//main function for error checking and program flow
+function main() {
+    var args = process.argv.slice(2);
+    if (args.length != 2) {
+        console.log("incorrect arguments");
+        console.error("Please input a repo owner and repo name");
+    } else {
+        var repoOwner = args[0];
+        var repoName = args[1];
+
+        gitHubAPI(repoOwner, repoName, function(err, obj) { console.log(obj); });
+    }
+}
+
+main();
